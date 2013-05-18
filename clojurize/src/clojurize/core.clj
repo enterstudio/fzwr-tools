4e0813db22d10c10fb7a03ae1233ea3bb1508e18
Ricardo Mendes <rokusu@gmail.com>
2013-05-18 07:46:36 +0100

    trying to figure stuff out

--- clojurize/src/clojurize/core.clj
+++ clojurize/src/clojurize/core.clj
@@ -2,12 +2,35 @@
   (:require [dk.ative.docjure.spreadsheet :as dj]))
 
 
-(->> (dj/load-workbook "../history.xlsx")
-     (dj/select-sheet "WRs")
-     (dj/select-columns {:A :course
-                         :B :type
-                         :C :time
-                         :D :player
-                         :E :date
-                         :F :ship
-                         :G :mode}))
+(def rows
+  (->> (dj/load-workbook "../history.xlsx")
+       (dj/select-sheet "WRs")
+       (dj/row-seq)))
+
+(defn mapped-rows
+  [[headers & rows]]
+  (let [columns (->> headers
+                     (dj/cell-seq)
+                     (map dj/read-cell))]
+       columns))
+
+
+(mapped-rows rows)
+
+
+(defn wr-rows
+      [worksheet]
+      (dj/select-columns {:A :course
+                          :B :type
+                          :C :time
+                          :D :player
+                          :E :date
+                          :F :ship
+                          :G :mode} worksheet))
+
+(def wr-sheet
+  (->> (dj/load-workbook "../history.xlsx")
+       (dj/select-sheet "WRs")))
+
+
+((wr-rows wr-sheet) 1)
